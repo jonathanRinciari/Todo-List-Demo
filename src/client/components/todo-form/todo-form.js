9463e87891e74b322a545842b67a275188476413
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-06-29 19:01:53 -0400

    feat(Todoform): updated styling and functionality

--- src/client/components/todo-form/todo-form.js
+++ src/client/components/todo-form/todo-form.js
@@ -67,7 +67,7 @@ class TodoForm extends React.Component {
    */
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form className={TodoForm.baseCls} onSubmit={this.onSubmit}>
         <input
           onChange={this.onChange}
           placeholder="Add new todo..."
