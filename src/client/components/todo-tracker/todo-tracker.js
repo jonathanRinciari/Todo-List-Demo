fa94b05c8f8d8ebcc05867817700d268aa6bae40
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-06-29 22:51:55 -0400

    feat(Todotracker): added banner for when all tasks are complete

--- src/client/components/todo-tracker/todo-tracker.js
+++ src/client/components/todo-tracker/todo-tracker.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Button from "../button/button";
+import Status from '../../assets/status.svg';
 
 
-const TodoTracker = (props) => {
+const TodoTracker = ({count, completeAllTasksHandler}) => {
   let baseClass = 'todo-tracker';
+
+  const renderBanner = () => {
+    return count ? (
+      <>
+        <span className={`${baseClass}-counter`}>{count} {count > 1 ? 'Tasks' : 'Task'} Remaining</span>
+        <Button onClick={completeAllTasksHandler} type="complete-all-tasks" text="Complete All"/>
+      </>
+    ) : (
+      <div className={`${baseClass}-all-tasks-complete`}>
+        <img src={Status} alt="done"/>
+        <span>All Tasks Complete!</span>
+      </div>
+    )
+  };
 
   return (
     <div className={baseClass}>
-      <span className={`${baseClass}-counter`}>2 Tasks Remaining</span>
-      <Button type="complete-all-tasks" text="Complete All"/>
+      {renderBanner()}
     </div>
   )
 };
