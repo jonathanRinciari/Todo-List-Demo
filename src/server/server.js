3498f319d91789e5f97b071eafc98fb7787f3ec9
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-06-30 16:38:29 -0400

    feat(Routes): added support for rendering bundle on multiple routes

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const statuses = {
   active: 'active',
   complete: 'complete',
   archived: 'archived'
-}
+};
 
 app.set('views', path.resolve('src', 'server', 'views'));
 app.set('view engine', 'ejs');
@@ -16,14 +16,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let todos = [
-  { id: 1, text: 'Hello, world!', status: statuses.active },
-  { id: 2, text: 'Pick up groceries', status: statuses.complete, archive: true}
+  { id: 1, text: 'Fix Bugs', status: statuses.complete, archive: false},
+  { id: 2, text: 'Style Client', status: statuses.complete, archive: false},
+  { id: 3, text: 'Implement Todo Status Tracker', status: statuses.complete, archive: false},
+  { id: 4, text: 'Implement Archive feature', status: statuses.complete, archive: false},
+  { id: 5, text: 'Integrate react router', status: statuses.complete, archive: false},
+  { id: 6, text: 'Walk the dog', status: statuses.active, archive: false},
+  { id: 7, text: 'Cook Dinner', status: statuses.active, archive: false},
+  { id: 8, text: 'Make breakfast', status: statuses.complete, archive: true},
 ];
-
-app.get('/', (req, res) => {
-  const bundle = `//${req.hostname}:8080/public/bundle.js`;
-  res.render('index', { bundle });
-});
 
 app.get('/todos', (req, res) => {
   res.json(JSON.stringify(todos));
@@ -64,11 +65,10 @@ app.post('/todos', (req, res) => {
     return;
   }
 
-  const id = todos.length + 1;
+  const id = todos.length === 0 ? 0 : todos[todos.length - 1].id + 1;
   const newTodo = { id, text, status: statuses.active };
 
   todos.push(newTodo);
-
   res.status(201).json(JSON.stringify(todos));
 });
 
@@ -96,6 +96,12 @@ app.put('/todos/:id', (req, res) => {
 
   todos.splice(index, 1, todo);
   res.status(200).json(JSON.stringify(todo));
+});
+
+
+app.get(['/', '/active', '/completed', '/archived'], (req, res) => {
+  const bundle = `//${req.hostname}:8080/public/bundle.js`;
+  res.render('index', { bundle });
 });
 
 // Node server.
